1c23ae77aae43f7ba5719941d82839f02e215bed
paul0701 <paul07011985@gmail.com>
2026-04-22 21:11:51 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'donationiq-v1';
+const ASSETS = [
+  '/Donate-IQ-/',
+  '/Donate-IQ-/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
